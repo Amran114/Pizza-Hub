484f747cfef87cfe592b336331742f2aca5ce49d
Amran-ahmed <amranahmed088@gmail.com>
2022-03-22 20:56:18 +0200

    added javascript code

--- JS/script.js
+++ JS/script.js
@@ -7,5 +7,71 @@ $(document).ready(function(){
       console.log (
           pizzatype + pizzasize + toppings + crusttype
     )
+    switch(pizzasize){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 1200;
+           console.log(price);
+         break;
+         case "medium":
+           price = 800;
+         break;
+         case "small":
+           price = 500;
+         default:
+           console.log("No price  listed"); 
+       }
+       switch(pizzatype){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 1200;
+           console.log(price);
+         break;
+         case "medium":
+           price = 800;
+         break;
+         case "small":
+           price = 500;
+         default:
+           console.log("No price  listed"); 
+       }
+       switch(toppings){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 1200;
+           console.log(price);
+         break;
+         case "medium":
+           price = 800;
+         break;
+         case "small":
+           price = 500;
+         default:
+           console.log("No price  listed"); 
+       }
+        switch(crusttype){
+        case "0":
+          crustprice =0;
+        break;
+        case "large":
+           crustprice= 1200;
+           console.log(price);
+         break;
+         case "medium":
+           crustprice = 800;
+         break;
+         case "small":
+           crustprice = 500;
+         default:
+           console.log("No price  listed"); 
+       }
+
+
     });   
   });
